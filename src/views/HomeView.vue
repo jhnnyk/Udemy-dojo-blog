42541d62bc62279ee9fb08bf567617196f14a7b9
John Knotts <jhnnyk@gmail.com>
2022-07-19 15:42:31 -0600

    remove defaults and look at Composition API basics

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup>
-import TheWelcome from "@/components/TheWelcome.vue";
+const name = 'john';
+const age = 45;
+
+const handleClick = () => {
+  console.log('you clicked me');
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    home
+    <p>my name is {{ name }} and my age is: {{ age }}</p>
+    <button @click="handleClick">click me</button>
   </main>
 </template>
