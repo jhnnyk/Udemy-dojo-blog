93eab33e1ebdc348e37f4481a0ab2ecc1f5e4837
John Knotts <jhnnyk@gmail.com>
2022-07-20 15:09:55 -0600

    computed values

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,29 @@
 <script setup>
-import { ref, reactive } from 'vue';
+import { ref, computed } from 'vue';
 
-const ninjaOne = ref({ name: 'mario', age: 30 });
-const ninjaTwo = reactive({ name: 'luigi', age: 35 });
+const search = ref('');
+const names = ref([
+  'mario',
+  'yoshi',
+  'luigi',
+  'toad',
+  'bowser',
+  'koopa',
+  'peach',
+]);
 
-const updateNinjaOne = () => {
-  ninjaOne.value.age = 40;
-};
-const updateNinjaTwo = () => {
-  ninjaTwo.age = 45;
-};
+const matchingNames = computed(() => {
+  return names.value.filter((name) => name.includes(search.value));
+});
 </script>
 
 <template>
   <main>
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
-    <button @click="updateNinjaOne">Update ninja one</button>
-    <hr />
-
-    <h2>Reactive</h2>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
-    <button @click="updateNinjaTwo">Update ninja two</button>
+    <input type="text" v-model="search" />
+    <p>search term: {{ search }}</p>
+    <ul>
+      <li v-for="name in matchingNames" :key="name">{{ name }}</li>
+    </ul>
   </main>
 </template>
